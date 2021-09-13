66d6c33f59166ebf6a643bc266c153f5485d64fc
matherusT <mateusz.bytner@gmail.com>
2021-09-13 20:24:03 +0200

    Add css

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(data_publikacji__lte = timezone.now()).order_by('data_publikacji')
+    return render(request, 'blog/post_list.html', { 'posts' : posts })
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
