f847481a9978ce58ad543c62a070019b798db180
matherusT <mateusz.bytner@gmail.com>
2021-09-15 21:14:59 +0200

    adding and editing post

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,10 +2,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from blog import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path('', views.post_list, name = 'post_list'),
+    path('post/<int:pk>/', views.post_detail, name = 'post_detail'),
+    path('post/new', views.new_post, name = 'new_post' ),
+    path('post/<int:pk>/edit', views.post_edit, name = 'post_edit'),
+
+
 ]
